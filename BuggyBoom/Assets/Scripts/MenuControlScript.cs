57390ebb02a22e8563bd58bbe69cea7277078e41
LordGee <gordon.johnson@mychaos.co.uk>
2019-10-08 11:45:21 +0100

    Upgrade to 2019.1 (Works)

--- BuggyBoom/Assets/Scripts/MenuControlScript.cs
+++ BuggyBoom/Assets/Scripts/MenuControlScript.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Experimental.UIElements;
 using UnityEngine.UI;
 using Slider = UnityEngine.UI.Slider;
 using Toggle = UnityEngine.UI.Toggle;
